bb4f07682bdeea4802dda33fe0b7acc59f632807
hanff <hanffeix@outlook.com>
2024-09-25 23:47:55 +0800

    fix

---
+++ sing-box.rb
@@ -0,0 +1,75 @@
+class SingBox < Formula
+  desc "Universal proxy platform"
+  homepage "https://sing-box.sagernet.org"
+  url "https://github.com/SagerNet/sing-box/archive/refs/tags/v1.9.6.tar.gz"
+  sha256 "09412fcc7b3a56c325886af090fb88b2cd8a61984856da43e2d16ff203f3a267"
+  license "GPL-3.0-or-later"
+  head "https://github.com/SagerNet/sing-box.git", branch: "dev-next"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/sagernet/sing-box/constant.Version=#{version} -buildid="
+    tags = "with_gvisor,with_quic,with_wireguard,with_utls,with_reality_server,with_clash_api"
+    system "go", "build", "-tags", tags, *std_go_args(ldflags:), "./cmd/sing-box"
+    generate_completions_from_executable(bin/"sing-box", "completion")
+  end
+
+  service do
+    system run [opt_bin/"sing-box", "run", "--config", etc/"sing-box/config.json", "--directory", var/"lib/sing-box"]
+    run_type :immediate
+    keep_alive true
+  end
+
+  test do
+    ss_port = free_port
+    (testpath/"shadowsocks.json").write <<~EOS
+      {
+        "inbounds": [
+          {
+            "type": "shadowsocks",
+            "listen": "::",
+            "listen_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    EOS
+    server = fork { exec bin/"sing-box", "run", "-D", testpath, "-c", testpath/"shadowsocks.json" }
+
+    sing_box_port = free_port
+    (testpath/"config.json").write <<~EOS
+      {
+        "inbounds": [
+          {
+            "type": "mixed",
+            "listen": "::",
+            "listen_port": #{sing_box_port}
+          }
+        ],
+        "outbounds": [
+          {
+            "type": "shadowsocks",
+            "server": "127.0.0.1",
+            "server_port": #{ss_port},
+            "method": "2022-blake3-aes-128-gcm",
+            "password": "8JCsPssfgS8tiRwiMlhARg=="
+          }
+        ]
+      }
+    EOS
+    system bin/"sing-box", "check", "-D", testpath, "-c", "config.json"
+    client = fork { exec bin/"sing-box", "run", "-D", testpath, "-c", "config.json" }
+
+    sleep 3
+    begin
+      system "curl", "--socks5", "127.0.0.1:#{sing_box_port}", "github.com"
+    ensure
+      Process.kill 9, server
+      Process.wait server
+      Process.kill 9, client
+      Process.wait client
+    end
+  end
+end
